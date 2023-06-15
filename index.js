3762f93c69a5b325d7d99de9337243e1e7ea8d3e
Barasa-Micah <barasamica@gmail.com>
2023-06-15 17:17:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply= num1*num2;
+
+const random = Math.floor(Math.random()*10) + 3;
+
+let num3= 54;
+let num4 =10;
+const mod = num3%num4;
+
+const max = Math.max(20,18,12,8,7,2,2,1);
